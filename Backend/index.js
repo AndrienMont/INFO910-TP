24cc91fd386c879f3ee72d8397e2422de52bbc06
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2024-11-02 17:05:34 +0100

    beginning of login

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,55 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http').Server(app);
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'INFO910';
+const client = new MongoClient(url);
+
+app.use(cors({ origin: "*"}));
+app.use(express.json());
 
 http.listen(3000, function(){
-    console.log('Server started');
+    console.log('Server started on port 3000');
 });
 
 app.get('/', function(req, res){
     res.send('Hello World');
+});
+
+app.get('/users', async function(req, res){
+    try {
+        await client.connect()
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+        const result = await collection.find().toArray();
+        console.log('Found the following records:');
+        console.log(result);        
+        res.status(200).json(result);
+    }catch (err) {
+        res.status(400).json({ message: err.message });
+    }finally{
+        client.close();
+    }
+});
+
+app.post('/register', async function(req, res){
+    try{
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db(dbName);
+        const collection = db.collection('users');
+        let newUser = req.body;
+        const result = await collection.insertOne(newUser);
+        console.log('Inserted new user');
+        console.log(result);
+        res.status(200).json(result);
+    }catch (err){
+        res.status(400).json({ message: err.message });
+    }finally{
+        client.close();
+    }
 });
---
+++ Frontend/src/routes/login/+page.svelte
@@ -0,0 +1,111 @@
+<script>
+    let username = '';
+    let password = '';
+    let confirmPassword = '';
+    let isRegistering = false;
+
+    const handleLogin = () => {
+        // Handle login logic here
+        console.log('Logging in with', username, password);
+        fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    };
+
+    const handleRegister = () => {
+        // Handle registration logic here
+        if (password !== confirmPassword) {
+            alert('Les mots de passe ne correspondent pas');
+            return;
+        } else {
+            fetch('http://localhost:3000/register', {
+                method: 'POST',
+                headers: {
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        }
+        console.log('Registering with', username, password, confirmPassword);
+    };
+</script>
+
+<style>
+    .container {
+        max-width: 400px;
+        margin: 0 auto;
+        padding: 2rem;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        text-align: center;
+    }
+
+    h1 {
+        margin-bottom: 1.5rem;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    input {
+        margin-bottom: 1rem;
+        padding: 0.5rem;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+    form button {
+        padding: 0.5rem 1rem;
+        background-color: #333;
+        color: white;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    .registerBtn {
+        padding-top: 1rem;
+        background: none; 
+        border: none; 
+        color: blue; 
+        text-decoration: underline; 
+        cursor: pointer;
+    }
+
+</style>
+    
+<div class="container">
+    <h1>{isRegistering ? 'Register' : 'Login'}</h1>
+    <form on:submit|preventDefault={isRegistering ? handleRegister : handleLogin}>
+        <input type="text" bind:value={username} placeholder="Username" />
+        <input type="password" bind:value={password} placeholder="Password" />
+        {#if isRegistering}
+            <input type="password" bind:value={confirmPassword} placeholder="Confirm Password" />
+        {/if}
+        <button type="submit">{isRegistering ? 'Register' : 'Login'}</button>
+    </form>
+    <button type="button" on:click={() => isRegistering = !isRegistering} class="registerBtn">
+        {isRegistering ? 'Already have an account ? Login' : 'Don\'t have an account? Register'}
+    </button>
+</div>
