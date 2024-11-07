377c5f3c34a047b3720973f996148f15b2983302
Andrien Montmayeur <92160718+AndrienMont@users.noreply.github.com>
2024-11-07 12:12:18 +0100

    displayed sellable estates

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -2,15 +2,33 @@
     import { onMount } from 'svelte';
 
     let username = '';
+    let estates = /** @type {Array<{name: string, price: number, location: string, superficy: number, image: string}>} */ ([]);
 
     onMount(() => {
         username = localStorage.getItem('username') || '';
+        fetchEstates();
     });
 
     const handleLogout = () => {
         localStorage.removeItem('username');
         window.location.reload();
     };
+
+    const fetchEstates = async () => {
+        fetch('http://localhost:3000/sellableEstates', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            estates = data;
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
 
 </script>
 
@@ -23,3 +41,13 @@
     <a href="/publish">Publish an estate</a>
 {/if}
 
+{#each estates as estate}
+    <div>
+        <h2>{estate.name}</h2>
+        <p>{estate.price} €</p>
+        <p>{estate.location}</p>
+        <p>{estate.superficy} m²</p>
+        <img src="data:image/png;base64,{estate.image}" alt="" />
+        <button>Buy</button>
+    </div>
+{/each}
